55fdbd1d02a97505248702f0376f2cddf63c78e8
M <8013994+emchin@users.noreply.github.com>
2020-06-12 22:47:12 -0700

    Add files via upload

---
+++ my_script.py
@@ -0,0 +1,15 @@
+"""Script to run some part of my project."""
+
+# This adds the directory above to our Python path
+#   This is so that we can add import our custom python module code into this script
+import sys
+sys.path.append('../')
+
+# Imports
+from my_module.functions import *
+
+###
+###
+
+# PYTHON SCRIPT HERE
+create_cloud()
